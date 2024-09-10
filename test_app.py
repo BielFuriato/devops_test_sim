b4f544af35d22283867b5e5295f36748cbef7641
BielFuriato <exitlag0808@gmail.com>
2024-09-09 21:55:07 -0300

    Criação de muita coisa

---
+++ test_app.py
@@ -0,0 +1,14 @@
+test_app.py
+
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    rv = client.get('/')
+    assert rv.data == b'Hello, World!'
